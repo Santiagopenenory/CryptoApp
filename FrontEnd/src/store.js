547826076e68b6c612940d99354192bcf1ec9181
Santiago <penenorysantiago@gmail.com>
2022-07-01 19:47:05 -0300

    Instalacion de auth0

--- FrontEnd/src/store.js
+++ FrontEnd/src/store.js
@@ -1,4 +1,4 @@
-import {writable} from 'svelte/store'
+import {writable,derived} from 'svelte/store'
 
 
 //Fetching de las cryptomonedas en general//
@@ -33,4 +33,20 @@ export const allCoins = writable([])
 
 
 
+//Auth0 Autentication//
+export const isAuthenticated = writable(false);
+export const user = writable({});
+export const popupOpen = writable(false);
+export const error = writable();
 
+export const tasks = writable([]);
+
+export const user_tasks = derived([tasks, user], ([$tasks, $user]) => {
+  let logged_in_user_tasks = [];
+
+  if ($user && $user.email) {
+    logged_in_user_tasks = $tasks.filter((task) => task.user === $user.email);
+  }
+
+  return logged_in_user_tasks;
+});
