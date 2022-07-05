c4c577baca4457bf1e274a52d758df7a7bfadb1a
Santiago <penenorysantiago@gmail.com>
2022-07-05 03:59:09 -0300

    BackEnd Dockerfile y docker-compose

--- BackEnd/src/database.js
+++ BackEnd/src/database.js
@@ -2,6 +2,6 @@
 const mongoose = require('mongoose')
 const url = ''
 
-mongoose.connect('mongodb://127.0.0.1:27017')
+mongoose.connect('mongodb://mongodb')
         .then(db => console.log('Db is connected to ',db.connection.host))
         .catch(err => console.error(err))
