c71928e3783c76eb6ecec93cdb7b16135ed00422
Santiago <penenorysantiago@gmail.com>
2022-07-05 13:56:03 -0300

    Configuracion de variables de entorno y db mongo

--- BackEnd/src/database.js
+++ BackEnd/src/database.js
@@ -1,7 +1,9 @@
 
 const mongoose = require('mongoose')
-const url = ''
 
-mongoose.connect('mongodb://mongodb')
+mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
         .then(db => console.log('Db is connected to ',db.connection.host))
         .catch(err => console.error(err))
--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-
+require('dotenv').config()
 require('./database')
 
 app.use(express.json());
